1919de56d30621f7ddf58d07d5015bb6791812f1
Samarth S <samarth.s@nutanix.com>
2019-05-20 16:10:43 +0530

    See the modulesexample folder.
    The hnnengine is the starting point for the project.
    This dynamically loads all the kernels in the directory 'kernels'
    A valid kernel file is specified by inserting a new file whose name is
    the name of the kernel and it defines exactly one function by the same name.
    After the parsing the dictonary called kernels will have the keys as the
    module names and the the value is the function object to that kernel

---
+++ modulesexample/kernels/tsp.py
@@ -0,0 +1,5 @@
+# This file defines the tsp function
+
+def tsp(*args, **kwargs):
+    print("This is the tsp kernel")
+
---
+++ modulesexample/kernels/vc.py
@@ -0,0 +1,5 @@
+# This defines the vertec cover kernel
+
+def vc(*args, **kwargs):
+    print("The vertex cover kernel")
+
